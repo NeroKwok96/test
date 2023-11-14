3475f6af089dc9741016be096f5c6b545ed8250a
Nero Kwok <nerokwok96@yahoo.com>
2023-11-15 00:04:39 +0800

    hi

--- test_model_copy.py
+++ test_model_copy.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import numpy as np
 import tensorflow as tf
-from models.low_vel_gan import LowVelGAN
+# from models.low_vel_gan import LowVelGAN
 
 #Define desired patterns for filtering the unwanted files
 allowed_fan_sensor_ids = ['189249773', '189265662', '189265907', '189265924', '189265970', '189286743', '189286744', '189286750', '189286774', '189286790', '189286793', '189286804', '189286837']
@@ -41,7 +41,7 @@ def calculate_machine_health(mae, threshold_good, threshold_usable, threshold_un
         machine_health = 4
     return machine_health
 
-fan_data_path = "data/vel/s3/fan/189249773/189249773_20230101_011247_vel_freq.csv"
+fan_data_path = "189249773_20230101_011247_vel_freq.csv"
 fan_data = pd.read_csv(fan_data_path)
 fan_data = fan_data.drop(columns=[fan_data.columns[-1]])
 # Fan balancing
@@ -57,41 +57,46 @@ fan_misalignment_actual_data = np.reshape(fan_misalignment_input_data, (1,224,2,
 # Fan belt
 fan_belt_df = fan_data.to_numpy()[:128, 1:]
 fan_belt_squared_values = np.square(fan_belt_df[:, 0:2])
-vertical_variance = np.abs(fan_belt_df[:, 0:1] - np.max(fan_belt_df[:, 0:1]))
-horizontal_variance = np.abs(fan_belt_df[:, 1:2] - np.max(fan_belt_df[:, 1:2]))
-axial_variance = np.abs(fan_belt_df[:, 2:] - np.max(fan_belt_df[:, 2:]))
-fan_belt_input_data = np.concatenate([fan_belt_df, fan_belt_squared_values, vertical_variance, horizontal_variance, axial_variance], axis=-1)
+fan_belt_vertical_variance = np.abs(fan_belt_df[:, 0:1] - np.max(fan_belt_df[:, 0:1]))
+fan_belt_horizontal_variance = np.abs(fan_belt_df[:, 1:2] - np.max(fan_belt_df[:, 1:2]))
+fan_belt_axial_variance = np.abs(fan_belt_df[:, 2:] - np.max(fan_belt_df[:, 2:]))
+fan_belt_input_data = np.concatenate([fan_belt_df, fan_belt_squared_values, fan_belt_vertical_variance, fan_belt_horizontal_variance, fan_belt_axial_variance], axis=-1)
 fan_belt_actual_data = np.reshape(fan_belt_input_data, (1,128,8,1))
 # Fan flow
 fan_flow_df_1 = fan_data.to_numpy()[884:884 + 42, 1:3]
 fan_flow_df_2 = fan_data.to_numpy()[1780:1780 + 43, 1:3]
 fan_flow_df_3 = fan_data.to_numpy()[2676:2676 + 43, 1:3]
-fan_flow_input_data = np.concatenate([fan_flow_df_1, fan_flow_df_2, fan_flow_df_3])
+fan_flow_input_data = np.concatenate([fan_flow_df_1, fan_flow_df_2, fan_flow_df_3], axis=0)
 fan_flow_actual_data = np.reshape(fan_flow_input_data, (1,128,2,1))
 # Fan bearing
-fan_bearing_df = fan_data.to_numpy()[]
-# Model Prediction
-fan_balancing_model = LowVelGAN(input_shape=fan_balancing_actual_data.shape)
-fan_balancing_model.load_keras_models(save_dir=fan_balancing_model_path)
-fan_balancing_prediction_result = fan_balancing_model.generator.predict(fan_balancing_actual_data)[1]
-fan_misalignment_model = LowVelGAN(input_shape=fan_misalignment_actual_data.shape)
-fan_misalignment_model.load_keras_models(save_dir=fan_misalignment_model_path)
-fan_misalignment_prediction_result = fan_misalignment_model.generator.predict(fan_misalignment_actual_data)[1]
-fan_belt_model = LowVelGAN(input_shape=fan_belt_actual_data.shape)
-fan_belt_model.load_keras_models(save_dir=fan_belt_model_path)
-fan_belt_prediction_result = fan_belt_model.generator.predict(fan_belt_actual_data)[1]
-fan_flow_model = LowVelGAN(input_shape=fan_flow_actual_data.shape)
-fan_flow_model.load_keras_models(save_dir=fan_flow_model_path)
-fan_flow_prediction_result = fan_flow_model.generator.predict(fan_flow_actual_data)[1]
-# Mae
-fan_balancing_mae = np.mean(np.absolute(np.subtract(fan_balancing_actual_data, fan_balancing_prediction_result)))
-fan_misalignment_mae = np.mean(np.absolute(np.subtract(fan_misalignment_actual_data, fan_misalignment_prediction_result)))
-fan_belt_mae = np.mean(np.absolute(np.subtract(fan_belt_actual_data, fan_belt_prediction_result)))
-fan_flow_mae = np.mean(np.absolute(np.subtract(fan_flow_actual_data, fan_flow_prediction_result)))
-print('fan_balancing_mae: ', fan_balancing_mae)
-print('fan_misalignment_mae: ', fan_misalignment_mae)
-print('fan_belt_mae: ', fan_belt_mae)
-print('fan_flow_mae: ', fan_flow_mae)
+fan_bearing_df = fan_data.to_numpy()[:2048, 1:]
+fan_bearing_squared_values = np.square(fan_bearing_df[:, 0:2])
+fan_bearing_vertical_variance = np.abs(fan_bearing_df[:, 0:1] - np.sqrt(np.sum(np.square(fan_belt_df[:, 0:1])) / 1.5))
+fan_bearing_horizontal_variance = np.abs(fan_bearing_df[:, 1:2] - np.sqrt(np.sum(np.square(fan_belt_df[:, 1:2])) / 1.5))
+fan_bearing_axial_variance = np.abs(fan_bearing_df[:, 2:] - np.sqrt(np.sum(np.square(fan_belt_df[:, 2:])) / 1.5))
+fan_bearing_input_data = np.concatenate([fan_bearing_df, fan_bearing_squared_values, fan_bearing_vertical_variance, fan_bearing_horizontal_variance, fan_bearing_axial_variance], axis=-1)
+# # Model Prediction
+# fan_balancing_model = LowVelGAN(input_shape=fan_balancing_actual_data.shape)
+# fan_balancing_model.load_keras_models(save_dir=fan_balancing_model_path)
+# fan_balancing_prediction_result = fan_balancing_model.generator.predict(fan_balancing_actual_data)[1]
+# fan_misalignment_model = LowVelGAN(input_shape=fan_misalignment_actual_data.shape)
+# fan_misalignment_model.load_keras_models(save_dir=fan_misalignment_model_path)
+# fan_misalignment_prediction_result = fan_misalignment_model.generator.predict(fan_misalignment_actual_data)[1]
+# fan_belt_model = LowVelGAN(input_shape=fan_belt_actual_data.shape)
+# fan_belt_model.load_keras_models(save_dir=fan_belt_model_path)
+# fan_belt_prediction_result = fan_belt_model.generator.predict(fan_belt_actual_data)[1]
+# fan_flow_model = LowVelGAN(input_shape=fan_flow_actual_data.shape)
+# fan_flow_model.load_keras_models(save_dir=fan_flow_model_path)
+# fan_flow_prediction_result = fan_flow_model.generator.predict(fan_flow_actual_data)[1]
+# # Mae
+# fan_balancing_mae = np.mean(np.absolute(np.subtract(fan_balancing_actual_data, fan_balancing_prediction_result)))
+# fan_misalignment_mae = np.mean(np.absolute(np.subtract(fan_misalignment_actual_data, fan_misalignment_prediction_result)))
+# fan_belt_mae = np.mean(np.absolute(np.subtract(fan_belt_actual_data, fan_belt_prediction_result)))
+# fan_flow_mae = np.mean(np.absolute(np.subtract(fan_flow_actual_data, fan_flow_prediction_result)))
+# print('fan_balancing_mae: ', fan_balancing_mae)
+# print('fan_misalignment_mae: ', fan_misalignment_mae)
+# print('fan_belt_mae: ', fan_belt_mae)
+# print('fan_flow_mae: ', fan_flow_mae)
 # fan_balancing_machine_health = calculate_machine_health(fan_balancing_mae, fan_balancing_threshold[0], fan_balancing_threshold[1], fan_balancing_threshold[2])
 
 # Script: docker run -it --rm --name my-running-app -v C:/Users/hikar/Documents/Code/xs-tswh-vel-spec-ai/out/models:/usr/src/app/out/models -v C:/Users/hikar/Documents/Code/xs-tswh-vel-spec-ai/data/vel/s3:/usr/src/app/data/vel/s3 -v C:/Users/hikar/Documents/Code/xs-tswh-vel-spec-ai/src:/usr/src/app/src -v C:/Users/hikar/Documents/Code/xs-tswh-vel-spec-ai/src/test_model_copy.py:/usr/src/app/src/test_model_copy.py xs-tensorflow-app python src/test_model_copy.py
